7b8530befa511278c80d25d5d16968cfbfdf8b63
piq9117 <piq9117@gmail.com>
2023-10-07 00:18:42 -0400

    bulk update

--- src/Server.hs
+++ src/Server.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 
 module Server (server) where
 
+import Network.Wai.Middleware.RequestLogger (logStdout)
 import Web.Scotty qualified as Scotty
 
 server :: Scotty.ScottyM () -> IO ()
-server scottyApp = Scotty.scotty 3000 scottyApp
+server scottyApp = Scotty.scotty 3000 $ do
+  Scotty.middleware logStdout
+  scottyApp
